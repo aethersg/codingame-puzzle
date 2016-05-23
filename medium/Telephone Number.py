2a9e93544b236d67e20dbb2d739cac7d1a088603
jude <jude@gogame.net>
2016-05-23 22:42:27 +0800

    added in telephone number

---
+++ medium/Telephone Number.py
@@ -0,0 +1,43 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(raw_input())
+telephone_list = []
+
+
+def make_tree(tl):
+    root = dict()
+    for t in tl:
+        current_dict = root
+        for no in t:
+            current_dict = current_dict.setdefault(no, {})
+    return root
+
+
+def count_child(d):
+    yield len(d)
+
+    for v in d.itervalues():
+        if isinstance(v, dict):
+            for x in count_child(v):
+                yield x
+
+
+def return_sum(d):
+    return sum(count_child(d))
+
+
+for i in xrange(n):
+    telephone = raw_input()
+    telephone_list.append(telephone)
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+
+# The number of elements (referencing a number) stored in the structure.
+
+tree = make_tree(telephone_list)
+count = return_sum(tree)
+print count
