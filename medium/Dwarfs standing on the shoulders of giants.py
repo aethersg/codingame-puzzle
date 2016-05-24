753c4c7627a30c7f4e2ced55d1956e31eb6a50aa
jude <jude@gogame.net>
2016-05-24 09:56:02 +0800

    added in dwarfts standing on the shoulders of giants

---
+++ medium/Dwarfs standing on the shoulders of giants.py
@@ -0,0 +1,43 @@
+import sys
+import math
+
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+class Node(object):
+    def __init__(self, id):
+        self.id, self.depth, self.affected_ppl = id, 1, []
+
+    def affected(self, n):
+        self.affected_ppl.append(n)
+
+    def update(self):
+        for n in self.affected_ppl:
+            if (n.depth < self.depth + 1):
+                n.depth = self.depth + 1
+                n.update()
+
+
+nodes = {}
+n = int(raw_input())  # the number of relationships of influence
+for i in xrange(n):
+    # x: a relationship of influence between two people (x influences y)
+    x, y = [int(j) for j in raw_input().split()]
+    if x not in nodes:
+        nodes[x] = Node(x)
+    if y not in nodes:
+        nodes[y] = Node(y)
+    nx, ny = nodes[x], nodes[y]
+    nx.affected(ny)
+    nx.update()
+
+max_depth = 0
+for j in nodes.values():
+    max_depth = max(j.depth, max_depth)
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+
+
+# The number of people involved in the longest succession of influences
+print max_depth
