903841c129c2a6fb3be9a1fb2108b8599ef670d9
jude <jude@gogame.net>
2016-07-17 15:20:55 +0800

    added in optimization for dont panic need to do better

---
+++ Python/code-golf/Dont Panic.py
@@ -0,0 +1,13 @@
+n,w,r,f,p,nc,ae,ne=[int(i) for i in raw_input().split()]
+m = {f:p}
+for i in range(ne):
+    ef, ep=[int(j) for j in raw_input().split()]
+    m[ef]=ep
+def l(a,b,c):
+    v=a-b.get(c)
+    return"LEFT"if v>0 else"RIGHT"if v<0 else"N"
+while 1:
+    cf,cp,d=raw_input().split()
+    cf=int(cf)
+    cp=int(cp)
+    print"WAIT" if(not(-1in[cp,cf])and d!="N")and l(cp,m,cf)in[d,"N"]else"BLOCK"
