a24f5fa5240216982c96baae4ff2939300bb21c3
jude <jude@gogame.net>
2016-07-17 15:38:15 +0800

    updated in optimization for power of thor need to do better

--- Python/code-golf/power_of_thor.py
+++ Python/code-golf/power_of_thor.py
@@ -1,8 +1,7 @@
-lx, ly, tx, ty = [int(i) for i in raw_input().split()]
-d = {(-1, 1): 'SW',(0, 1): 'S',(1, 1): 'SE',(-1, 0): 'W',(0, 0): None,(1, 0): 'E',(-1, -1): 'NW',(0, -1): 'N',(1, -1): 'NE'}
-while tx != lx or ty != ly:
-    rt = int(raw_input())
-    dx, dy = map(lambda x: (x[0] > x[1]) - (x[0] < x[1]), ((lx, tx), (ly, ty)))
-    tx += dx
-    ty += dy
-    print(d.get((dx, dy)))
+x,y,j,k=[int(i) for i in raw_input().split()]
+d={(-1,1):'SW',(0,1):'S',(1,1):'SE',(-1,0):'W',(0,0):None,(1,0):'E',(-1,-1):'NW',(0,-1):'N',(1,-1):'NE'}
+while j!=x or k!=y:
+    l,m=map(lambda a:(a[0]>a[1])-(a[0]<a[1]),((x,j),(y,k)))
+    j+=l
+    k+=m
+    print d.get((l,m))
