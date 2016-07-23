d7c0468fba966dca2ebbee7aad50eb773dfa5a78
jude <jude@gogame.net>
2016-07-23 13:25:34 +0800

    add in hard super computer

---
+++ Python/hard/Super Computer.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(raw_input())
+array = []
+last_date = 0
+for i in xrange(n):
+    j, d = [int(j) for j in raw_input().split()]
+    array.append((j, j + d))
+    last_date = max(last_date, array[i][1])
+
+array.sort(key=lambda a: a[1])
+
+timeline = [0] * last_date
+answer = 0
+for a in array:
+    if not 1 in timeline[a[0]:a[1]]:
+        answer += 1
+        timeline[a[0]:a[1]] = [1] * (a[1] - a[0])
+
+print answer
