9b851f0aa412052006672cc715d00f7ccc354ce8
jude <jude@affle.com>
2019-12-28 17:22:08 +0800

    added in Code_vs_Zombie.py updated README.md

--- Python3/medium/There_Is_No_Spoon_episode_1.py
+++ Python3/medium/There_Is_No_Spoon_episode_1.py
@@ -1,13 +1,10 @@
-import sys
-import math
-
 # Don't let the machines win. You are humanity's last hope...
 
-width = int(raw_input())  # the number of cells on the X axis
-height = int(raw_input())  # the number of cells on the Y axis
+width = int(input())  # the number of cells on the X axis
+height = int(input())  # the number of cells on the Y axis
 grid = []
-for i in xrange(height):
-    line = raw_input()  # width characters, each either 0 or .
+for i in range(height):
+    line = input()  # width characters, each either 0 or .
     grid.append(list(line))
 
 # Write an action using print
@@ -15,22 +12,22 @@ for i in xrange(height):
 
 # Three coordinates: a node, its right neighbor, its bottom neighbor
 
-for y in xrange(height):
-    for x in xrange(width):
+for y in range(height):
+    for x in range(width):
         if grid[y][x] == '0':
-            print '%s %s' % (x, y),
+            print('%s %s' % (x, y), end=" ")
 
             xx = 1
             while ((x + xx) < width) and grid[y][x + xx] == '.':
                 xx += 1
             if (x + xx) < width:
-                print '%s %s' % ((x + xx), y),
+                print('%s %s' % ((x + xx), y), end=" ")
             else:
-                print '-1 -1',
+                print('-1 -1', end=" ")
             yy = 1
             while ((y + yy) < height) and grid[y + yy][x] == '.':
                 yy += 1
             if (y + yy) < height:
-                print '%s %s' % (x, (y + yy))
+                print('%s %s' % (x, (y + yy)))
             else:
-                print '-1 -1'
+                print('-1 -1')
