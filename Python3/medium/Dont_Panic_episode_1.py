65045d4234d5d5c31013ad577abacaaf4eae6d4c
jude <jude@affle.com>
2019-12-28 21:08:25 +0800

    added in Dont_Panic_episode_1.py updated README.md

--- Python3/medium/Dont_Panic_episode_1.py
+++ Python3/medium/Dont_Panic_episode_1.py
@@ -1,6 +1,3 @@
-import sys
-import math
-
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
@@ -12,13 +9,13 @@ import math
 # nb_total_clones: number of generated clones
 # nb_additional_elevators: ignore (always zero)
 # nb_elevators: number of elevators
-nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in raw_input().split()]
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
 
 elevator_mapping = {exit_floor: exit_pos}
-for i in xrange(nb_elevators):
+for i in range(nb_elevators):
     # elevator_floor: floor on which this elevator is found
     # elevator_pos: position of the elevator on its floor
-    elevator_floor, elevator_pos = [int(j) for j in raw_input().split()]
+    elevator_floor, elevator_pos = [int(j) for j in input().split()]
     # creating a map of the elevator this assumes that there is only one per floor
     elevator_mapping[elevator_floor] = elevator_pos
 
@@ -43,7 +40,7 @@ while True:
     # clone_floor: floor of the leading clone
     # clone_pos: position of the leading clone on its floor
     # direction: direction of the leading clone: LEFT or RIGHT
-    clone_floor, clone_pos, direction = raw_input().split()
+    clone_floor, clone_pos, direction = input().split()
     clone_floor = int(clone_floor)
     clone_pos = int(clone_pos)
     # Write an action using print
@@ -51,8 +48,8 @@ while True:
     # action: WAIT or BLOCK
     if leading_clone(clone_floor, clone_pos, direction):
         if direction == find_direction_to_lift(clone_pos, elevator_mapping, clone_floor) or find_direction_to_lift(clone_pos, elevator_mapping, clone_floor) == "NONE":
-            print "%s" % "WAIT"
+            print("%s" % "WAIT")
         else:
-            print "%s" % "BLOCK"
+            print("%s" % "BLOCK")
     else:
-        print "%s" % "WAIT"
+        print("%s" % "WAIT")
