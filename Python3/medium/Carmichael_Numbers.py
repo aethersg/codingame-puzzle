3431bd8bc8d41a0bd195e82b68ec9d4ff33b440e
jude <jude@affle.com>
2020-01-01 22:12:30 +0800

    updated Carmichael_Numbers.py

--- Python3/medium/Carmichael_Numbers.py
+++ Python3/medium/Carmichael_Numbers.py
@@ -10,27 +10,17 @@ n = int(input())
 # Write an action using print
 # To debug: print >> sys.stderr, "Debug messages..."
 def gcd(a, b):
-    if (a < b):
+    if a < b:
         return int(gcd(b, a))
-    if (a % b == 0):
+    if a % b == 0:
         return b
     return int(gcd(b, a % b))
-
-
-def power(x, y, mod):
-    if (y == 0):
-        return 1
-    temp = power(x, int(y/2), mod) % mod
-    temp = (temp * temp) % mod
-    if (y % 2 == 1):
-        temp = (temp * x) % mod
-    return temp
 
 
 def checkprime(p):
     flag = True
     for i in range(2, p):
-        if (p % i) == 0:
+        if int(p % i) == 0:
             flag = False
             break
     return flag
@@ -44,11 +34,11 @@ def icm(n):
     while b < n:
 
         # If "b" is relatively prime to n
-        if (int(gcd(b, n)) == 1):
+        if int(gcd(b, n)) == 1:
 
             # And pow(b, n-1)% n is not 1,
             # return false.
-            if (int(power(b, n - 1, n)) != 1):
+            if int(pow(b, n - 1, n)) != 1:
                 return 0
         b = b + 1
     return 1
